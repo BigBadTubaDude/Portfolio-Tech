371891d7208e3f2f26495b71fe6e25b11594d2dd
BigBadTubaDude <cralex0307@gmail.com>
2025-01-02 18:55:32 -0500

    added filter bar for categories

---
+++ Portfolio_Tech/src/components/CategoryFilterBar.tsx
@@ -0,0 +1,30 @@
+import { usePortfolioEntries } from "../hooks/fetchHooks/usePortfolioEntries";
+interface CategoryFilterBarProps {
+    selectedCategories: string[];
+    setSelectedCategories: React.Dispatch<React.SetStateAction<string[]>>;
+  }
+
+export function CategoryFilterBar({
+    selectedCategories, setSelectedCategories
+} : CategoryFilterBarProps) {
+    const { data: portfolioEntries } = usePortfolioEntries();
+    const projectCategories: string[] = portfolioEntries?.categories || []
+
+
+    return (
+        <header>
+            <nav>
+                {
+                    projectCategories.map(category => {
+                        return (<button className={selectedCategories.includes(category) ? "selected" : ""}
+                            key={category}
+                            onClick={() => setSelectedCategories((old: string[]) => old.includes(category) ? old.filter((item: string) => item !== category) : [category, ...old])}
+                        >
+                            {category}
+                        </button>)
+                    })
+                }
+            </nav>
+        </header>
+    )
+}
--- Portfolio_Tech/src/components/Projects.tsx
+++ Portfolio_Tech/src/components/Projects.tsx
@@ -1,13 +1,17 @@
 import { Category } from "./Category"
-import { usePortfolioEntries } from "../hooks/fetchHooks/usePortfolioEntries";
+import { CategoryFilterBar } from "./CategoryFilterBar";
+import { useState } from "react";
 
 export default function Projects() {
-    const { data: portfolioEntries, isLoading} = usePortfolioEntries();
-    // Get list of unique categories the projects fall under
-    const projectCategories : string[] =  portfolioEntries?.categories || []
+    // Get list of unique categories the projects fall under    
+    const [selectedCategories, setSelectedCategories] = useState<string[]>([])
     return (
         <main>
-                {isLoading ? "Loading..." : projectCategories.map((category: string)=> <Category key={category} category={category}/>)}
+                <CategoryFilterBar 
+                        selectedCategories={selectedCategories}
+                        setSelectedCategories={setSelectedCategories}
+                />
+                {selectedCategories.map((category: string)=> <Category key={category} category={category} />)}
         </main>
 )
 }
