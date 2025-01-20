98a41bf5abc10479fce7bc899ee25f6afb0a356f
BigBadTubaDude <cralex0307@gmail.com>
2025-01-20 15:29:57 -0500

    added icons for skills

--- Portfolio_Tech/src/components/ProjectSummary.tsx
+++ Portfolio_Tech/src/components/ProjectSummary.tsx
@@ -1,11 +1,18 @@
 import { ProjectEntry } from "../types"
-export function ProjectSummary({project}: {project: ProjectEntry}) {
-    const dateWorkedOn = new Date(project.dateRangeStart)
-    const monthWorkedOn = dateWorkedOn.getMonth()
+import { icons } from "../assets/skillsIcons";
+export function ProjectSummary({ project }: { project: ProjectEntry }) {
+    const dateWorkedOn = new Date(project.publishedDate)
+    const yearWorkedOn = dateWorkedOn.getFullYear()
+    const monthWorkedOn = dateWorkedOn.toLocaleString('default', { month: 'long' });
+
+    const skillsUsed = project.skillsUsed || []
     console.log(project)
     return (
-        <>
-            <p>{monthWorkedOn}</p>
-        </>
+        <footer className="project__summary">
+            <p>{`${monthWorkedOn} ${yearWorkedOn}`}</p>
+            <ul>
+                {skillsUsed.map(skill => <li key={skill}><img src={icons[skill] || ""} /></li>)}
+            </ul>
+        </footer>
     )
 }
