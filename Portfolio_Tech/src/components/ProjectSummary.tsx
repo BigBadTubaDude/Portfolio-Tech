ea1a2e049e496244b7ba0c28a14ccac5f27b467d
BigBadTubaDude <cralex0307@gmail.com>
2025-01-20 16:30:51 -0500

    added icons to project summary footer

--- Portfolio_Tech/src/components/ProjectSummary.tsx
+++ Portfolio_Tech/src/components/ProjectSummary.tsx
@@ -5,14 +5,12 @@ export function ProjectSummary({ project }: { project: ProjectEntry }) {
     const yearWorkedOn = dateWorkedOn.getFullYear()
     const monthWorkedOn = dateWorkedOn.toLocaleString('default', { month: 'long' });
 
-    const skillsUsed = project.skillsUsed || []
+    const skillsUsed = project.skillsUsed.sort((a: string, b: string) => a.localeCompare(b)) || []
     console.log(project)
     return (
         <footer className="project__summary">
             <p>{`${monthWorkedOn} ${yearWorkedOn}`}</p>
-            <ul>
-                {skillsUsed.map(skill => <li key={skill}><img src={icons[skill] || ""} /></li>)}
-            </ul>
+            {skillsUsed.map(skill => icons[skill] ? <img key={skill} src={icons[skill] || ""}/> : null)}
         </footer>
     )
 }
